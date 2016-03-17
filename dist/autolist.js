48f86a2094bc6caba2c06370c283555544cc1c90
Varun Raj <mailme@varunraj.in>
2016-03-17 15:04:19 +0530

    Extension Enabled Check

--- dist/autolist.js
+++ dist/autolist.js
@@ -6,11 +6,11 @@ var AutoList = MediumEditor.Extension.extend({
   onKeypress: function (keyPressEvent) {
    if (MediumEditor.util.isKey(keyPressEvent, [MediumEditor.util.keyCode.SPACE])) {
         var list_start = this.base.getSelectedParentElement().textContent;
-        if (list_start == "1."){
+        if (list_start == "1."  && this.base.getExtensionByName('orderedlist')){
           this.base.execAction('insertorderedlist');
           this.base.getSelectedParentElement().textContent = this.base.getSelectedParentElement().textContent.slice(2).trim();
         }
-        else if( list_start == "*"){
+        else if( list_start == "*" && this.base.getExtensionByName('unorderedlist')){
           this.base.execAction('insertunorderedlist');
           this.base.getSelectedParentElement().textContent = this.base.getSelectedParentElement().textContent.slice(1).trim();
         }
